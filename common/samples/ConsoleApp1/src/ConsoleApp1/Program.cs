sachmo912uh/AspNetCore.Docs

commit df6fbe96536d25828cc74252e48f0f355713688e

Updates common samples to beta7

common/samples/ConsoleApp1/src/ConsoleApp1/Program.cs
@@ -9,7 +9,6 @@ namespace ConsoleApp1
     {
         public void Main(string[] args)
         {
-            Console.WriteLine("Hello from DNX!");
         }
     }
 }

common/samples/ClassLibrary1/ClassLibrary1.sln
@@ -3,15 +3,15 @@ Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 14
 VisualStudioVersion = 14.0.23107.0
 MinimumVisualStudioVersion = 10.0.40219.1
-Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{6CD161D5-C204-4E67-8F91-BA2998A9752F}"
+Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{DB348ABF-99A8-4ECF-AD3A-5A5FD8F788CF}"
 EndProject
-Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{548776B4-E410-4F66-ACD2-65975473BD9F}"
+Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{87C589E4-A849-4F3C-8D22-B26BCC173EFA}"
 	ProjectSection(SolutionItems) = preProject
 		global.json = global.json
 		NuGet.Config = NuGet.Config
 	EndProjectSection
 EndProject
-Project("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}") = "ClassLibrary1", "src\ClassLibrary1\ClassLibrary1.xproj", "{AAE7AB77-C8A8-49D7-BE70-9EFC7363C67D}"
+Project("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}") = "ClassLibrary1", "src\ClassLibrary1\ClassLibrary1.xproj", "{DA0C4BE8-CCFD-452C-ADB8-5C3576212ADD}"
 EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
@@ -19,15 +19,15 @@ Global
 		Release|Any CPU = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{AAE7AB77-C8A8-49D7-BE70-9EFC7363C67D}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{AAE7AB77-C8A8-49D7-BE70-9EFC7363C67D}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{AAE7AB77-C8A8-49D7-BE70-9EFC7363C67D}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{AAE7AB77-C8A8-49D7-BE70-9EFC7363C67D}.Release|Any CPU.Build.0 = Release|Any CPU
+		{DA0C4BE8-CCFD-452C-ADB8-5C3576212ADD}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{DA0C4BE8-CCFD-452C-ADB8-5C3576212ADD}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{DA0C4BE8-CCFD-452C-ADB8-5C3576212ADD}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{DA0C4BE8-CCFD-452C-ADB8-5C3576212ADD}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
 	GlobalSection(NestedProjects) = preSolution
-		{AAE7AB77-C8A8-49D7-BE70-9EFC7363C67D} = {6CD161D5-C204-4E67-8F91-BA2998A9752F}
+		{DA0C4BE8-CCFD-452C-ADB8-5C3576212ADD} = {DB348ABF-99A8-4ECF-AD3A-5A5FD8F788CF}
 	EndGlobalSection
 EndGlobal

common/samples/ClassLibrary1/src/ClassLibrary1/ClassLibrary1.xproj
@@ -7,7 +7,7 @@
 
   <Import Project="$(VSToolsPath)\DNX\Microsoft.DNX.Props" Condition="'$(VSToolsPath)' != ''" />
   <PropertyGroup Label="Globals">
-    <ProjectGuid>aae7ab77-c8a8-49d7-be70-9efc7363c67d</ProjectGuid>
+    <ProjectGuid>da0c4be8-ccfd-452c-adb8-5c3576212add</ProjectGuid>
     <RootNamespace>ClassLibrary1</RootNamespace>
     <BaseIntermediateOutputPath Condition="'$(BaseIntermediateOutputPath)'=='' ">..\..\artifacts\obj\$(MSBuildProjectName)</BaseIntermediateOutputPath>
     <OutputPath Condition="'$(OutputPath)'=='' ">..\..\artifacts\bin\$(MSBuildProjectName)\</OutputPath>

common/samples/ConsoleApp1/ConsoleApp1.sln
@@ -3,15 +3,15 @@ Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 14
 VisualStudioVersion = 14.0.23107.0
 MinimumVisualStudioVersion = 10.0.40219.1
-Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{2F6A52B6-20D2-4816-B9F1-51ADFD1F293F}"
+Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{F25AAA4C-5436-4435-886E-8675DEA167CA}"
 EndProject
-Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{5CD577D0-B785-4B49-9948-513F49E7E2E2}"
+Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{B3BB47BB-193C-45C3-9B61-41DB4DD2AA3F}"
 	ProjectSection(SolutionItems) = preProject
 		global.json = global.json
 		NuGet.Config = NuGet.Config
 	EndProjectSection
 EndProject
-Project("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}") = "ConsoleApp1", "src\ConsoleApp1\ConsoleApp1.xproj", "{C7F348D4-5A6F-48F0-B12F-B21043D4ED86}"
+Project("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}") = "ConsoleApp1", "src\ConsoleApp1\ConsoleApp1.xproj", "{A254ECA9-BD76-45D5-AE62-7D2E99EDDB20}"
 EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
@@ -19,15 +19,15 @@ Global
 		Release|Any CPU = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{C7F348D4-5A6F-48F0-B12F-B21043D4ED86}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{C7F348D4-5A6F-48F0-B12F-B21043D4ED86}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{C7F348D4-5A6F-48F0-B12F-B21043D4ED86}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{C7F348D4-5A6F-48F0-B12F-B21043D4ED86}.Release|Any CPU.Build.0 = Release|Any CPU
+		{A254ECA9-BD76-45D5-AE62-7D2E99EDDB20}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{A254ECA9-BD76-45D5-AE62-7D2E99EDDB20}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{A254ECA9-BD76-45D5-AE62-7D2E99EDDB20}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{A254ECA9-BD76-45D5-AE62-7D2E99EDDB20}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
 	GlobalSection(NestedProjects) = preSolution
-		{C7F348D4-5A6F-48F0-B12F-B21043D4ED86} = {2F6A52B6-20D2-4816-B9F1-51ADFD1F293F}
+		{A254ECA9-BD76-45D5-AE62-7D2E99EDDB20} = {F25AAA4C-5436-4435-886E-8675DEA167CA}
 	EndGlobalSection
 EndGlobal

common/samples/ConsoleApp1/NuGet.Config
@@ -1,6 +1,8 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
 <configuration>
   <packageSources>
+    <!--To inherit the global NuGet package sources remove the <clear/> line below -->
+    <clear />
     <add key="api.nuget.org" value="https://api.nuget.org/v3/index.json" />
   </packageSources>
 </configuration>

common/samples/ConsoleApp1/src/ConsoleApp1/ConsoleApp1.xproj
@@ -7,7 +7,7 @@
 
   <Import Project="$(VSToolsPath)\DNX\Microsoft.DNX.Props" Condition="'$(VSToolsPath)' != ''" />
   <PropertyGroup Label="Globals">
-    <ProjectGuid>c7f348d4-5a6f-48f0-b12f-b21043d4ed86</ProjectGuid>
+    <ProjectGuid>a254eca9-bd76-45d5-ae62-7d2e99eddb20</ProjectGuid>
     <RootNamespace>ConsoleApp1</RootNamespace>
     <BaseIntermediateOutputPath Condition="'$(BaseIntermediateOutputPath)'=='' ">..\..\artifacts\obj\$(MSBuildProjectName)</BaseIntermediateOutputPath>
     <OutputPath Condition="'$(OutputPath)'=='' ">..\..\artifacts\bin\$(MSBuildProjectName)\</OutputPath>

common/samples/WebApplication1/WebApplication1.sln
@@ -3,15 +3,15 @@ Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 14
 VisualStudioVersion = 14.0.23107.0
 MinimumVisualStudioVersion = 10.0.40219.1
-Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{4A4B9500-D6B3-4E87-85CC-2A2A39E4F9C1}"
+Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{EFCB49A9-DEBC-4D28-9DA3-95AA35105DDC}"
 EndProject
-Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{97E8154F-D956-4827-ABEE-278FF73CD27C}"
+Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{5DC4364E-0E4C-4ECA-A5A7-3AEE299F9660}"
 	ProjectSection(SolutionItems) = preProject
 		global.json = global.json
 		NuGet.Config = NuGet.Config
 	EndProjectSection
 EndProject
-Project("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}") = "WebApplication1", "src\WebApplication1\WebApplication1.xproj", "{ED014861-FAB4-424A-8D0D-D740A23F8F22}"
+Project("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}") = "WebApplication1", "src\WebApplication1\WebApplication1.xproj", "{AC65DB1B-CE24-41EB-B194-2C461F33CA85}"
 EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
@@ -19,15 +19,15 @@ Global
 		Release|Any CPU = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{ED014861-FAB4-424A-8D0D-D740A23F8F22}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{ED014861-FAB4-424A-8D0D-D740A23F8F22}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{ED014861-FAB4-424A-8D0D-D740A23F8F22}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{ED014861-FAB4-424A-8D0D-D740A23F8F22}.Release|Any CPU.Build.0 = Release|Any CPU
+		{AC65DB1B-CE24-41EB-B194-2C461F33CA85}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{AC65DB1B-CE24-41EB-B194-2C461F33CA85}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{AC65DB1B-CE24-41EB-B194-2C461F33CA85}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{AC65DB1B-CE24-41EB-B194-2C461F33CA85}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
 	GlobalSection(NestedProjects) = preSolution
-		{ED014861-FAB4-424A-8D0D-D740A23F8F22} = {4A4B9500-D6B3-4E87-85CC-2A2A39E4F9C1}
+		{AC65DB1B-CE24-41EB-B194-2C461F33CA85} = {EFCB49A9-DEBC-4D28-9DA3-95AA35105DDC}
 	EndGlobalSection
 EndGlobal

common/samples/WebApplication1/src/WebApplication1/Controllers/AccountController.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Principal;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Authentication;
 using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Data.Entity;
-using Microsoft.Data.Entity.Infrastructure;
-using WebApplication1;
 using WebApplication1.Models;
 using WebApplication1.Services;
+using WebApplication1.ViewModels.Account;
 
 namespace WebApplication1.Controllers
 {
@@ -446,7 +443,7 @@ namespace WebApplication1.Controllers
             if (!_databaseChecked)
             {
                 _databaseChecked = true;
-                context.Database.ApplyMigrations();
+                context.Database.Migrate();
             }
         }
 

commit 69c912d0476954ccd3a392689a4c564ee0fa839a

Add missing output. Fixes #719
Fixes a regression in df6fbe96536d25828cc74252e48f0f355713688e.

common/samples/ConsoleApp1/src/ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -9,6 +6,7 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
+			Console.WriteLine("Hello from DNX!");
         }
     }
 }